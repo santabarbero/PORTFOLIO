9063323d65ea7790025e5615f1025fdf7f67e001
santabarbero <67341547+santabarbero@users.noreply.github.com>
2022-05-10 14:43:35 +0200

    Nuestro primer Hola Mundo en Flask

--- prueba.py
+++ prueba.py
@@ -1,13 +1,13 @@
-#Importar
 from flask import Flask
-app = Flask(__name__)
+app = Flask("__name__")
 @app.route("/")
 def holamundo():
-    return "Hola mundo¡"
-@app.route('/mis-proyectos')
+    return "holamundo"
+#@app.route('/mis-proyectos')
 def mostrarproyectos():
     return 'Aquí se mostrarán mis proyectos'
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
 
- 
+    
+    
